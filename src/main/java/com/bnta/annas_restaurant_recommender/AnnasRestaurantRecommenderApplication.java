9ced22f2cd89e30d9662637f445de19195bb2187
cbattenplowright <callumbp15@gmail.com>
2023-08-14 17:22:21 +0100

    initial commit

---
+++ src/main/java/com/bnta/annas_restaurant_recommender/AnnasRestaurantRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.bnta.annas_restaurant_recommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnasRestaurantRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnasRestaurantRecommenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnta/annas_restaurant_recommender/AnnasRestaurantRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnta.annas_restaurant_recommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnasRestaurantRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
