9694b2a047336da576eecc29a5bdb94f823fc388
nehcobiy <yibo-chen@hotmail.com>
2023-08-15 15:30:09 +0100

    finished get dishes by id endpoint

--- src/main/java/com/bnta/annas_restaurant_recommender/controllers/DishController.java
+++ src/main/java/com/bnta/annas_restaurant_recommender/controllers/DishController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("dishes")
@@ -21,6 +23,17 @@ public class DishController {
     @GetMapping
     public ResponseEntity<List<Dish>> getAllDishes(){
         return new ResponseEntity<>(dishService.findAllDishes(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<Dish>> getDishById(@PathVariable Long id){
+        Optional<Dish> foundDish = dishService.findDishById(id);
+
+        if (foundDish.isPresent()){
+            return new ResponseEntity<>(foundDish, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
 
     }
 
