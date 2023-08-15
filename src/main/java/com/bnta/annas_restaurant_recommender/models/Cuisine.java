7f05e5ba9ed21f3f00a5a78aa50fa0f70f7f85ed
Sandra <sandramtzd@hotmail.com>
2023-08-15 11:42:40 +0100

    cuisine enum complete

--- src/main/java/com/bnta/annas_restaurant_recommender/models/Cuisine.java
+++ src/main/java/com/bnta/annas_restaurant_recommender/models/Cuisine.java
@@ -1,4 +1,35 @@
 package com.bnta.annas_restaurant_recommender.models;
 
 public enum Cuisine {
+
+    AMERICAN ("American"),
+    CHINESE ("Chinese"),
+    FRENCH ("French"),
+    GREEK ("Greek"),
+    INDIAN ("Indian"),
+    ITALIAN("Italian"),
+    JAPANESE("Japanese"),
+    KENYAN ("Kenyan"),
+    KOREAN ("Korean"),
+    MEDITERRANEAN ("Mediterranean"),
+    MEXICAN ("Mexican"),
+    NIGERIAN("Nigerian"),
+    PAKISTANI("Pakistani"),
+    THAI("Thai"),
+    TURKISH("Turkish"),
+    SOMALIAN("Somalian"),
+    VIETNAMESE("Vietnamese"),
+    ZIMBABWEAN ("Zimbabwean");
+
+    private final String displayCuisineName;
+
+    Cuisine (String displayCuisineName){
+        this.displayCuisineName = displayCuisineName;
+    }
+
+    public String getCuisineName(){
+        return displayCuisineName;
+    }
+
+
 }
