be8ed7ba94a60809cc088dd19c7f1e490218fab7
amelieboydshire <aboydshire10@gmail.com>
2023-08-15 10:45:21 +0100

    completed dish model

--- src/main/java/com/bnta/annas_restaurant_recommender/models/Dish.java
+++ src/main/java/com/bnta/annas_restaurant_recommender/models/Dish.java
@@ -1,4 +1,107 @@
 package com.bnta.annas_restaurant_recommender.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table( name = "dishes")
 public class Dish {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private boolean vegetarian;
+
+    @Column
+    private boolean vegan;
+
+    @Column (name = "dairy_free")
+    private boolean dairyFree;
+
+    @Column
+    private boolean halal;
+
+
+    @JsonIgnoreProperties({"dishes"})
+    @ManyToMany(mappedBy = "dishes")
+    private List<Cuisine> cuisines;
+
+
+
+    public Dish(String name, boolean vegetarian, boolean vegan, boolean dairyFree, boolean halal, List<Cuisine> cuisines) {
+        this.name = name;
+        this.vegetarian = vegetarian;
+        this.vegan = vegan;
+        this.dairyFree = dairyFree;
+        this.halal = halal;
+        this.cuisines = cuisines;
+    }
+
+    public Dish(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+
+    public void setVegetarian(boolean vegetarian) {
+        this.vegetarian = vegetarian;
+    }
+
+    public boolean isVegan() {
+        return vegan;
+    }
+
+    public void setVegan(boolean vegan) {
+        this.vegan = vegan;
+    }
+
+    public boolean isDairyFree() {
+        return dairyFree;
+    }
+
+    public void setDairyFree(boolean dairyFree) {
+        this.dairyFree = dairyFree;
+    }
+
+    public boolean isHalal() {
+        return halal;
+    }
+
+    public void setHalal(boolean halal) {
+        this.halal = halal;
+    }
+
+    public List<Cuisine> getCuisines() {
+        return cuisines;
+    }
+
+    public void setCuisines(List<Cuisine> cuisines) {
+        this.cuisines = cuisines;
+    }
 }
