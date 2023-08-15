4e5eacf024526fb65987153414dacf9a6a1943ad
Faran71 <muhammadfaran01.mfs@gmail.com>
2023-08-15 11:07:31 +0100

    price_range Enum complete

--- src/main/java/com/bnta/annas_restaurant_recommender/models/PriceRange.java
+++ src/main/java/com/bnta/annas_restaurant_recommender/models/PriceRange.java
@@ -1,4 +1,18 @@
 package com.bnta.annas_restaurant_recommender.models;
 
 public enum PriceRange {
+    LOW("£"),
+    MEDIUM("££"),
+    HIGH("£££");
+
+    private final String displayPrice;
+
+    PriceRange(String displayPrice){
+        this.displayPrice = displayPrice;
+    }
+
+    public String getDisplayPrice(){
+        return displayPrice;
+    }
 }
+
