aa961d69842a25b5a60989044e6a3ebccb08a5e9
cbattenplowright <callumbp15@gmail.com>
2023-08-15 11:31:30 +0100

    completed Borough enum

--- src/main/java/com/bnta/annas_restaurant_recommender/models/Borough.java
+++ src/main/java/com/bnta/annas_restaurant_recommender/models/Borough.java
@@ -1,4 +1,47 @@
 package com.bnta.annas_restaurant_recommender.models;
 
 public enum Borough {
+
+    BARKINGANDDAGENHAM("Barking and Dagenham"),
+    BARNET("Barnet"),
+    BEXLEY("Bexley"),
+    BRENT("Brent"),
+    BROMLEY("Bromley"),
+    CAMDEN("Camden"),
+    CROYDON("Croydon"),
+    EALING("Ealing"),
+    ENFIELD("Enfield"),
+    GREENWICH("Greenwich"),
+    HACKNEY("Hackney"),
+    HAMMERSMITHANDFULHAM("Hammersmith and Fulham"),
+    HARINGEY("Haringey"),
+    HARROW("Harrow"),
+    HAVERING("Havering"),
+    HILLINGDON("Hillingdon"),
+    HOUNSLOW("Hounslow"),
+    ISLINGTON("Islington"),
+    KENSINGTONANDCHELSEA("Kensington and Chelsea"),
+    KINGSTONUPONTHAMES("Kingston upon Thames"),
+    LAMBETH("Lambeth"),
+    LEWISHAM("Lewisham"),
+    MERTON("Merton"),
+    NEWHAM("Newham"),
+    REDBRIDGE("Redbridge"),
+    RICHMONDUPONTHAMES("Richmond upon Thames"),
+    SOUTHWARK("Southwark"),
+    SUTTON("Sutton"),
+    TOWERHAMLETS("Tower Hamlets"),
+    WALTHAMFOREST("Waltham Forest"),
+    WANDSWORTH("Wandsworth"),
+    WESTMINSTER("Westminster");
+
+    private final String displayBoroughName;
+
+    Borough(String displayBoroughName) {
+        this.displayBoroughName = displayBoroughName;
+    }
+
+    public String getBoroughName() {
+        return displayBoroughName;
+    }
 }
