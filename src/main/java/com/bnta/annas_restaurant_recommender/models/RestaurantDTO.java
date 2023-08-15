7d5be16dd42ed2cb733a3cf177737c674da4f029
cbattenplowright <callumbp15@gmail.com>
2023-08-15 11:12:35 +0100

    completed RestaurantDTO

--- src/main/java/com/bnta/annas_restaurant_recommender/models/RestaurantDTO.java
+++ src/main/java/com/bnta/annas_restaurant_recommender/models/RestaurantDTO.java
@@ -1,4 +1,73 @@
 package com.bnta.annas_restaurant_recommender.models;
 
+import java.util.List;
+
 public class RestaurantDTO {
+
+    private String name;
+    private Borough borough;
+    private PriceRange priceRange;
+    private int rating;
+    private List<Long> cuisineIds;
+    private List<Long> dishIds;
+
+    public RestaurantDTO(String name, Borough borough, PriceRange priceRange, int rating, List<Long> cuisineIds, List<Long> dishIds) {
+        this.name = name;
+        this.borough = borough;
+        this.priceRange = priceRange;
+        this.rating = rating;
+        this.cuisineIds = cuisineIds;
+        this.dishIds = dishIds;
+    }
+
+    public RestaurantDTO() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Borough getBorough() {
+        return borough;
+    }
+
+    public void setBorough(Borough borough) {
+        this.borough = borough;
+    }
+
+    public PriceRange getPriceRange() {
+        return priceRange;
+    }
+
+    public void setPriceRange(PriceRange priceRange) {
+        this.priceRange = priceRange;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public List<Long> getCuisineIds() {
+        return cuisineIds;
+    }
+
+    public void setCuisineIds(List<Long> cuisineIds) {
+        this.cuisineIds = cuisineIds;
+    }
+
+    public List<Long> getDishIds() {
+        return dishIds;
+    }
+
+    public void setDishIds(List<Long> dishIds) {
+        this.dishIds = dishIds;
+    }
 }
