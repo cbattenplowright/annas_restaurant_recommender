67ff474a039551b1e204c22828f2576f04828c9b
Sandra <sandramtzd@hotmail.com>
2023-08-15 16:02:38 +0100

    Post restaurant complete

--- src/main/java/com/bnta/annas_restaurant_recommender/services/RestaurantService.java
+++ src/main/java/com/bnta/annas_restaurant_recommender/services/RestaurantService.java
@@ -1,11 +1,14 @@
 package com.bnta.annas_restaurant_recommender.services;
 
+import com.bnta.annas_restaurant_recommender.models.Dish;
 import com.bnta.annas_restaurant_recommender.models.Restaurant;
+import com.bnta.annas_restaurant_recommender.models.RestaurantDTO;
 import com.bnta.annas_restaurant_recommender.repositories.DishRepository;
 import com.bnta.annas_restaurant_recommender.repositories.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,6 +26,17 @@ public class RestaurantService {
 
     public Restaurant findRestaurant(Long id){
         return restaurantRepository.findById(id).get();
+    }
+
+    public void saveRestaurant (RestaurantDTO restaurantDTO){
+        List<Dish> dishes = new ArrayList<>();
+        for (Long dishId : restaurantDTO.getDishIds()){
+            dishes.add(dishRepository.findById(dishId).get());
+        }
+        Restaurant restaurant = new Restaurant(restaurantDTO.getName(), restaurantDTO.getBorough(), restaurantDTO.getPriceRange(), restaurantDTO.getRating(), dishes);
+
+        restaurantRepository.save(restaurant);
+
     }
 
 }
